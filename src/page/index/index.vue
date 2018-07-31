8d4c7e23ce33616ccc8850f6c00e58fb6be1d2f9
fulei <fulei@taoshouyou.com>
2018-07-31 17:12:09 +0800

    初始化app

---
+++ src/page/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>this is index !</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function() {
+            return {
+                a:1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
