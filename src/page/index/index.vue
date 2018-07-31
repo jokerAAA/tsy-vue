dd367798c5b2d9f71105ab8915da466b098956e6
fulei <fulei@taoshouyou.com>
2018-07-31 17:37:59 +0800

    修改fetch方法

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -5,11 +5,27 @@
 </template>
 
 <script>
+    import {getIndexData } from '../../service/getData';
     export default {
         data:function() {
             return {
-                a:1
+                a:1,
+                data:{}
             }
+        },
+
+        created() {
+            this.initData()
+        },
+
+        methods:{
+
+            async initData() {
+               let data  = await getIndexData();
+            },
+
+            
+
         }
     }
 </script>
---
+++ src/service/getData.js
@@ -0,0 +1,7 @@
+import fetch from '../utils/fetch';
+
+export const getIndexData = () => {
+   return fetch('/indexpage/index/index');
+}
+
+export const a = 12 ;
