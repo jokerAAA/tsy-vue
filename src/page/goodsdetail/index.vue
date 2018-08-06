4f22db8a96111adb68c893f2851497acd9212229
fulei <fulei@taoshouyou.com>
2018-08-06 16:46:53 +0800

    添加商品详情页标题

--- src/page/goodsdetail/index.vue
+++ src/page/goodsdetail/index.vue
@@ -1,17 +1,22 @@
 <template>
-    <h1>this is goodsdetail</h1>
-
-
+    <div class="container">
+        <Goodstitle :item="trade"></Goodstitle>
+    </div>
 </template>
 
 <script>
-    // import axios from '@/util/axios';
+    import axios from '@/utils/axios';
+    import Goodstitle from '../common/goodstitle'
 
     export default {
         data:function() {
             return {
-
+                trade:{}, //商品相关
             }
+        },
+
+        components:{
+            Goodstitle
         },
 
         created() {
@@ -20,7 +25,23 @@
 
         methods:{
             init() {
+                const id = this.$route.query.id;
+                console.log(id);
+                this.getData(id);
+            },
 
+            getData(id) {
+                const that = this ;
+                axios.get('/api/trades/list/info',{
+                    params:{
+                        id:id
+                    }
+                })
+                .then(function(res) {
+                    let data = res.data.data;
+                    console.log(data);
+                    that.trade = data.trade;
+                })
             }
         }
     }
