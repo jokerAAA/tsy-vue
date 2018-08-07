f6b7ac355e1f63ffe3a35b15231d913ab0801455
fulei <fulei@taoshouyou.com>
2018-08-07 11:05:30 +0800

    添加筛选框，调整mask样式

--- src/page/goodslist/index.vue
+++ src/page/goodslist/index.vue
@@ -56,6 +56,93 @@
                 </div>
             </div>
         </div>
+        <!-- 筛选项 -->
+    <div class="mask" v-show="showType == 'info'" @click="closeMask" id="info">
+        <div class="info" :class="{slide:showType == 'info'}" @click.stop="stopPropagation">
+            <div class="info-top">
+                <div class="info-top-tip">
+                    绑定身份证商品，存在账号被找回风险，请玩家谨慎购买！
+                </div>
+                <div class="info-top-title">
+                    身份证
+                </div>
+                <div class="info-top-content">
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindcertificate == 2}" data-key="isbindcertificate" data-value="2" bindtap="chooseInfo">不限</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindcertificate == 1}" data-key="isbindcertificate" data-value="1" bindtap="chooseInfo">已绑定</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindcertificate == 0}" data-key="isbindcertificate" data-value="0" bindtap="chooseInfo">未绑定</span>
+                    </div>
+                </div>
+                <div class="info-top-title">
+                    手机
+                </div>
+                <div class="info-top-content">
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindmobile == 2}" data-key="isbindmobile" data-value="2" bindtap="chooseInfo">不限</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindmobile == 1}" data-key="isbindmobile" data-value="1" bindtap="chooseInfo">已绑定</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindmobile == 0}" data-key="isbindmobile" data-value="0" bindtap="chooseInfo">未绑定</span>
+                    </div>
+                </div>
+                <div class="info-top-title">
+                    邮箱
+                </div>
+                <div class="info-top-content">
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindemail == 2}" data-key="isbindemail" data-value="2" bindtap="chooseInfo">不限</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindemail == 1}" data-key="isbindemail" data-value="1" bindtap="chooseInfo">已绑定</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.isbindemail == 0}" data-key="isbindemail" data-value="0" bindtap="chooseInfo">未绑定</span>
+                    </div>
+                </div>
+                <div class="info-top-title">
+                    价格
+                </div>
+                <div class="info-top-content">
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 0}" data-key="price" data-value="0" bindtap="chooseInfo">不限</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 1}" data-key="price" data-value="1" bindtap="chooseInfo">0-50</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 2}" data-key="price" data-value="2" bindtap="chooseInfo">50-100</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 3}" data-key="price" data-value="3" bindtap="chooseInfo">100-500</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 4}" data-key="price" data-value="4" bindtap="chooseInfo">500-1000</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 5}" data-key="price" data-value="5" bindtap="chooseInfo">1000-2000</span>
+                    </div>
+                    <div class="info-top-content-items">
+                        <span class="info-top-content-items-text" :class="{'active-info':query.price == 6}" data-key="price" data-value="6" bindtap="chooseInfo">2000以上</span>
+                    </div>
+                </div>
+            </div>
+            <div class="info-bottom">
+                <div class="info-bottom-button" bindtap="resetInfo" id="reset">
+                    重置选项
+                </div>
+                <div class="info-bottom-button" bindtap="confirmInfo">
+                    确定
+                </div>
+            </div>
+        </div>
+    </div>
+
     </section>
 
 </template>
@@ -75,7 +162,11 @@ export default {
         page : 1 ,
         sort: 0 ,
         clientid:'',
-        areaid:''
+        areaid:'',
+        isbindmobile:2,
+        isbindcertificate:2,
+        isbindemail:2,
+        price:0
       },
       sortText:'排序',
       showType: '' ,
@@ -211,11 +302,12 @@ export default {
 
 .container {
     text-align: left;
+    padding-top:85px;
 }
 
 .mask {
-    top:40px;
-    height:calc(100% - 40px);
+    top:85px;
+    height:calc(100% - 85px);
 }
 
 /* 菜单栏 */
@@ -223,7 +315,11 @@ export default {
 	height:40px;
 	line-height: 40px;
 	border-bottom: 1px solid #f2f3f6;
-	display: flex;
+    display: flex;
+    position: fixed;
+    top:45px;
+    width:100%;
+    background:#fff;
 }
 
 .menu-item {
@@ -352,5 +448,89 @@ export default {
 
 .sort-item-name {
     font-size: 14px;
+}
+
+/* 筛选框 */
+/* 筛选下拉 */
+.info {
+    width:90%;
+    height:100%;
+    background:#fff;
+    animation:slideLeft 0.2s linear;
+    position: absolute;
+    top:0;
+    right:0;    
+}
+
+.info-top {
+    color:#666;
+    padding:10px 20px;
+    font-size: 12px;
+    height:calc(100% - 70px);
+}
+
+.info-top-tip {
+    margin-bottom:12px;
+}
+
+.info-top-title {
+    font-size: 15px;
+    height:20px;
+    line-height: 20px;
+}   
+
+.info-top-content {
+    margin:12px 12px 0 0;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.info-top-content-items {
+    width:33.33%;
+    text-align: center;
+    margin-bottom:12px;
+    display: flex;
+}
+
+.info-top-content-items-text {
+    width:80px;
+    height:30px;
+    line-height: 30px;
+    border:1px solid #666;
+    border-radius: 3px;
+}
+
+.active-info {
+    border-color:#F74B3D;
+    color:#F74B3D;
+}
+
+.info-bottom {
+    position: absolute;
+    bottom:0;
+    left:0;
+    width:100%;
+    height:45px;
+    line-height: 45px;
+    display: flex;
+    border-top:1rpx solid #f2f3f6;
+}
+
+.info-bottom-button {
+    flex:1;
+    font-size: 14px;
+    text-align: center;
+}
+
+.info-bottom-button:nth-of-type(1) {
+    background:#fff;
+    color:#333;
+    
+}
+
+.info-bottom-button:nth-of-type(2) {
+    background: #F74B3D;
+    color:#fff;
+
 }
 </style>
