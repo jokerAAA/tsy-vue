5530b8f9200133d6ff3f973b7a4b260524805430
fulei <fulei@taoshouyou.com>
2018-08-07 16:08:49 +0800

    修改热门游戏，修改reset文件

---
+++ src/page/index/children/hotgame.vue
@@ -0,0 +1,61 @@
+<template>  
+    <div class="hot">
+        <router-link class="hot-item" v-for="item in list" :key="item.id" :to="{path:'/goods/list',query:{gameid:item.id,goodsid:1}}">
+            <div class="hot-item-img">
+                <img :src="item.pic" alt="">
+            </div>
+            <p class="hot-item-text">{{item.name}}</p>
+        </router-link>
+        <router-link class="more" to="/game/list">查看更多游戏</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function() {  
+            return {
+
+            }
+        },
+        props:[
+            'list'
+        ]
+    }
+</script>
+
+<style scoped lang='less'>
+
+    .hot {
+        display: flex;
+        flex-wrap: wrap;
+        padding:5px;
+
+        &-item {
+            border-radius: 12px;
+            width:50%;
+            padding:5px;
+            margin-bottom:5px;
+            background:#f2f3f6;
+
+            &-img {
+                height:86px;
+                background: #fff;
+            }
+            &-text {
+                height:32px;
+                line-height: 32px;
+                font-size: 14px;
+                color:#333;
+            }
+        }
+    }
+
+    .more {
+        height:44px;
+        line-height: 44px;
+        background:#fff;
+        color:#666;
+        font-size: 14px;
+        width:100%;
+    }
+</style>
--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,34 +1,55 @@
 <template>
     <div class="container">
-        <h1>this is index !</h1>
+        <!-- 轮播图 -->
+        <div class="swiper">
+            this is swiper!
+        </div>
+        <!-- tab -->
+        <div class="tab">
+            <div class="tab-items">
+                <span class="tab-items-text">性价比推荐</span>
+            </div>
+            <div class="tab-items">
+                <span class="tab-items-text">热门游戏</span>
+            </div>
+        </div>
+        <!-- 性价比推荐 -->
+
+        <!-- 热门游戏 -->
+        <HotGame :list="hotGame"></HotGame>
     </div>
 </template>
 
 <script>
-    // import {getIndexData } from '../../service/getData';
-    // import axios from "@/uitls/axios";
+    import HotGame from './children/hotgame';
     import axios from 'axios';
     export default {
         data:function() {
             return {
-                a:1,
-                data:{}
+                swiper:[],
+                hotGame:[],
+                hotGoods:[]
             }
         },
+
+        components:{
+            HotGame
+        },
+
 
         created() {
             this.initData()
         },
 
         methods:{
-
-            // async initData() {
-            //    let data  = await getIndexData();
-            // },
             initData() {
                 axios.get('/api/indexpage/index/index')
-                .then(function(res) {
-                    console.log(res);
+                .then((res) => {
+                    const data = res.data.data;
+                    console.log(data);
+                    this.swiper = data.bannerList;
+                    this.hotGame = data.hotGameList;
+                    this.hotGoods = data.highQualityTradeList;
                 })
             }
 
@@ -38,6 +59,28 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang='less'>
 
+.swiper {
+    height:125px;
+    line-height: 125px;
+    background:#fff;
+    margin-bottom:10px;
+}
+
+.tab {
+    background:#fff;
+    height:50px;
+    line-height: 50px;
+    display: flex;
+    &-items {
+        width:50%;
+        text-align: center;
+        &-text {
+            display: inline-block;
+            height:100%;
+            font-size: 16px;
+        }
+    }
+}
 </style>
